f58224e08e41da34dd62c3ac3a163d2c2340f1ea
Chris Creighton <creighcl6510@gmail.com>
2019-05-10 16:19:31 -0400

    scene setup

---
+++ Assets/TMPPANCAM.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TMPPANCAM : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera vcam;
+    [SerializeField] float adjustmentAmount = 0.5f;
+
+    public void MOVE()
+    {
+        CinemachineTrackedDolly dolly = vcam.GetCinemachineComponent<CinemachineTrackedDolly>();
+        float curPos = dolly.m_PathPosition;
+        vcam.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = Mathf.Clamp(curPos + adjustmentAmount, 0f, 3f);
+    }
+}
---
+++ Assets/rotates.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotates : MonoBehaviour
+{
+    [SerializeField] float yRotation = 1f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, yRotation * Time.deltaTime);
+    }
+}
